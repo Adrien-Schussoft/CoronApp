aa11f283ac770c712d5e702d1c516523c13919f2
Adrien <adrien.dessartre@gmail.com>
2021-01-12 14:16:20 +0100

    Major typo correction

---
+++ shade/CoronApp.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCeDJGqQ+0E7KRxRcCCHKiaoA6Yg2u3o/OWSsE4YR68WdY7c36eyCeMc5FbYUZcm32hYgZhcVVZrdx+7YhZ5mnpbumGBeu2dpQ6vQ0uGhg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJHGL+UcMLB5CcC2MKG62Crw21ML6+euhmnE+W/Awdo7s07CJJfQvxErxY5Mswzpel9gfBR5KQjGkfAA6iH5SineNNsOZjyzuWEeHx1sgVWBsggM=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+java -jar CoronApp.jar
